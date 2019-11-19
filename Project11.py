cdf589f0464aaf10f21f0d9ec7cf295b0460e277
Draius <41041066+draius@users.noreply.github.com>
2019-11-19 15:45:36 -0500

    Init
    Sphere program

---
+++ Project11.py
@@ -0,0 +1,30 @@
+#Draius Disimone Lab 11
+
+#import math lib
+import math
+
+class Sphere:
+    
+#defining __init method
+    def __init__(self , radius):
+        self.radius = radius
+        self.surfaceArea = (4/3) * math.pi * self.radius ** 3
+        self.volume = 4 * math.pi * self.radius ** 2
+#defining radius        
+    def getRadius(self):
+        return self.radius
+#surface area        
+    def getSurfaceArea(self):
+        return self.surfaceArea
+#volume        
+    def getVolume(self):
+        return self.volume
+        
+def run():
+    radius = float(input("Enter the radius of your sphere: "))
+    obj = Sphere(radius)
+    print("VOLUME of Sphere = " , obj.getVolume())
+    print("SURFACE AREA of Sphere = " , obj.getSurfaceArea())
+    
+if __name__ == '__main__':
+    run()
